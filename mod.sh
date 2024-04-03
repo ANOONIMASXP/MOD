c5a29a0ae595599a49bd56b5abf1d3bf687d91ea
Nekonata <Nekonata@example.com>
2024-04-03 15:55:05 +0800

    up

--- mod.sh
+++ mod.sh
@@ -1,52 +1,52 @@
 #!/bin/bash
 
-curl -o cnip1.txt https://raw.githubusercontent.com/17mon/china_ip_list/master/china_ip_list.txt
-curl -o cnip2.txt https://raw.githubusercontent.com/gaoyifan/china-operator-ip/ip-lists/china.txt
-curl -o cnsite.txt https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/accelerated-domains.china.conf
-curl -o nocnsite.txt https://raw.githubusercontent.com/gfwlist/gfwlist/master/gfwlist.txt
+#curl -o cnip1.txt https://raw.githubusercontent.com/17mon/china_ip_list/master/china_ip_list.txt
+#curl -o cnip2.txt https://raw.githubusercontent.com/gaoyifan/china-operator-ip/ip-lists/china.txt
+#curl -o cnsite.txt https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/accelerated-domains.china.conf
+#curl -o nocnsite.txt https://raw.githubusercontent.com/gfwlist/gfwlist/master/gfwlist.txt
 curl -o ad.txt https://raw.githubusercontent.com/Mujinniao/list/main/domain.txt
 
 
 
-cat cnip*.txt | sort -u > cn_ip_cidr.txt
+#cat cnip*.txt | sort -u > cn_ip_cidr.txt
 
-rm cnip1.txt cnip2.txt
-
-
-
-sed -i -e 's/^server=\///' -e 's/\/114.114.114.114//' -e '/^#/d' -e '/^$/d' cnsite.txt
-
-sort -u cnsite.txt > cn_domain.txt
-
-sed 's/^/./' cnsite.txt > cn_domain_suffix.txt
-
-rm cnsite.txt
+#rm cnip1.txt cnip2.txt
 
 
 
-base64 -d nocnsite.txt > temp.txt && mv temp.txt nocnsite.txt
+#sed -i -e 's/^server=\///' -e 's/\/114.114.114.114//' -e '/^#/d' -e '/^$/d' cnsite.txt
 
-sed -i -e '/^\[/d' -e '/^!/d' -e '/^$/d' nocnsite.txt
+#sort -u cnsite.txt > cn_domain.txt
 
-grep '^@@' nocnsite.txt > white.txt && sed -i -e '/^@@/d' -e '/^$/d' nocnsite.txt
+#sed 's/^/./' cnsite.txt > cn_domain_suffix.txt
 
-sed -i 's/^@@//' white.txt
+#rm cnsite.txt
 
-sort nocnsite.txt -o nocnsite.txt
 
-sort white.txt -o white.txt
 
-comm -23 nocnsite.txt white.txt > temp.txt && mv temp.txt nocnsite.txt
+#base64 -d nocnsite.txt > temp.txt && mv temp.txt nocnsite.txt
 
-sed -i -e 's/^||//' -e 's/^|//' -e 's/https:\/\///' -e 's/http:\/\///' -e 's/\/.*//' nocnsite.txt
+#sed -i -e '/^\[/d' -e '/^!/d' -e '/^$/d' nocnsite.txt
 
-sed -e '/\*/d' -e '/^$/d' -e '/^[^.]/s/^/./' nocnsite.txt > nocn_domain_suffix.txt
+#grep '^@@' nocnsite.txt > white.txt && sed -i -e '/^@@/d' -e '/^$/d' nocnsite.txt
 
-sed -e '/^\./d' -e '/\*/d' -e '/^$/d' nocnsite.txt > nocn_domain.txt
+#sed -i 's/^@@//' white.txt
 
-grep '\*' nocnsite.txt | sed -e 's/\./\\./g' -e 's/\*/\.\*/g' > nocn_domain_regex.txt
+#sort nocnsite.txt -o nocnsite.txt
 
-rm nocnsite.txt white.txt
+#sort white.txt -o white.txt
+
+#comm -23 nocnsite.txt white.txt > temp.txt && mv temp.txt nocnsite.txt
+
+#sed -i -e 's/^||//' -e 's/^|//' -e 's/https:\/\///' -e 's/http:\/\///' -e 's/\/.*//' nocnsite.txt
+
+#sed -e '/\*/d' -e '/^$/d' -e '/^[^.]/s/^/./' nocnsite.txt > nocn_domain_suffix.txt
+
+#sed -e '/^\./d' -e '/\*/d' -e '/^$/d' nocnsite.txt > nocn_domain.txt
+
+#grep '\*' nocnsite.txt | sed -e 's/\./\\./g' -e 's/\*/\.\*/g' > nocn_domain_regex.txt
+
+#rm nocnsite.txt white.txt
 
 
 
@@ -66,24 +66,24 @@ rm ad.txt
 
 
 
-jq --slurpfile domain <(cat cn_domain.txt | jq -R .) '.rules[0].domain += $domain' mod.json > temp.json
+#jq --slurpfile domain <(cat cn_domain.txt | jq -R .) '.rules[0].domain += $domain' mod.json > temp.json
 
-jq --slurpfile domain_suffix <(cat cn_domain_suffix.txt | jq -R .) '.rules[0].domain_suffix += $domain_suffix' temp.json > temp1.json
+#jq --slurpfile domain_suffix <(cat cn_domain_suffix.txt | jq -R .) '.rules[0].domain_suffix += $domain_suffix' temp.json > temp1.json
 
-jq --slurpfile ip_cidr <(cat cn_ip_cidr.txt | jq -R .) '.rules[0].ip_cidr += $ip_cidr' temp1.json > cn.json
+#jq --slurpfile ip_cidr <(cat cn_ip_cidr.txt | jq -R .) '.rules[0].ip_cidr += $ip_cidr' temp1.json > cn.json
 
-rm cn_ip_cidr.txt cn_domain*.txt temp*.json
-
-
+#rm cn_ip_cidr.txt cn_domain*.txt temp*.json
 
 
-jq --slurpfile domain <(cat nocn_domain.txt | jq -R .) '.rules[0].domain += $domain' mod.json > temp.json
 
-jq --slurpfile domain_suffix <(cat nocn_domain_suffix.txt | jq -R .) '.rules[0].domain_suffix += $domain_suffix' temp.json > temp1.json
 
-jq --slurpfile domain_regex <(cat nocn_domain_regex.txt | jq -R .) '.rules[0].domain_regex += $domain_regex' temp1.json > nocn.json
+#jq --slurpfile domain <(cat nocn_domain.txt | jq -R .) '.rules[0].domain += $domain' mod.json > temp.json
 
-rm nocn_domain*.txt temp*.json
+#jq --slurpfile domain_suffix <(cat nocn_domain_suffix.txt | jq -R .) '.rules[0].domain_suffix += $domain_suffix' temp.json > temp1.json
+
+#jq --slurpfile domain_regex <(cat nocn_domain_regex.txt | jq -R .) '.rules[0].domain_regex += $domain_regex' temp1.json > nocn.json
+
+#rm nocn_domain*.txt temp*.json
 
 
 
@@ -107,9 +107,9 @@ rm ad_ip_cidr.txt ad_domain*.txt temp*.json
 
 tar -xzvf sing-box.tar.gz
 
-./sing-box-1.8.5-linux-amd64/sing-box rule-set compile --output cn.srs cn.json
+#./sing-box-1.8.5-linux-amd64/sing-box rule-set compile --output cn.srs cn.json
 
-./sing-box-1.8.5-linux-amd64/sing-box rule-set compile --output nocn.srs nocn.json
+#./sing-box-1.8.5-linux-amd64/sing-box rule-set compile --output nocn.srs nocn.json
 
 ./sing-box-1.8.5-linux-amd64/sing-box rule-set compile --output ad.srs ad.json
 
