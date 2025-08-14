60cf17ca37eb7bf55084c04beab0c3aa568eefd7
Anekonata <Anekonata@example.com>
2025-08-15 00:45:51 +0800

    up

--- mod.sh
+++ mod.sh
@@ -37,49 +37,84 @@ sed -i -e 's/regexp://' -e 's/\$//' cnsite1_regexp.txt
 
 
 #处理site2
-sed -i -e 's/^server=\///' -e 's/\/114.114.114.114//' -e '/^#/d' -e '/^$/d' cnsite2.txt
+sed -e 's/^server=\///' -e 's/\/114.114.114.114//' -e '/^#/d' -e '/^$/d' cnsite2.txt > cnsite2_suffix.txt
 #合并去重后缀
-cat cnsite2.txt cnsite1_suffix.txt | sort -u > cnsite_suffix.txt
-#合并去重全名
-cat cnsite_suffix.txt cnsite1_domain.txt | sort -u > cn_domain.txt
+cat cnsite2_suffix.txt cnsite1_suffix.txt | sort -u > cnsite_suffix.txt
+
+#合并去重全名(sing-box)
+cat cnsite_suffix.txt cnsite1_domain.txt | sort -u > cn_domain-s.txt
+
+mv cnsite1_domain.txt cnsite1_domain-m.txt
+
 #处理符合sing-box的后缀规则
-sed 's/^/\.&/g' cnsite_suffix.txt > cn_domain_suffix.txt
+sed 's/^/\./g' cnsite_suffix.txt > cn_domain_suffix-s.txt
 
-rm cnsite1.txt cnsite2.txt cnsite1_domain.txt cnsite1_suffix.txt cnsite_suffix.txt
+#处理符合mihomo的后缀规则
+sed 's/^/\+./g' cnsite_suffix.txt > cn_domain_suffix-m.txt
+#合并
+cat cn_domain_suffix-m.txt  cnsite1_domain-m.txt > cn_domain-m.txt
 
-
-
-#sort -u cnsite.txt > cn_domain.txt
-
-#sed 's/^/./' cnsite.txt > cn_domain_suffix.txt
-
-#rm cnsite.txt
+rm cnsite1.txt cnsite2.txt cnsite1_domain.txt cnsite1_suffix.txt cnsite2_suffix.txt cnsite_suffix.txt cnsite1_regexp.txt cnsite1_domain-m.txt cn_domain_suffix-m.txt
 
 
 
-#base64 -d nocnsite.txt > temp.txt && mv temp.txt nocnsite.txt
 
-#sed -i -e '/^\[/d' -e '/^!/d' -e '/^$/d' nocnsite.txt
 
-#grep '^@@' nocnsite.txt > white.txt && sed -i -e '/^@@/d' -e '/^$/d' nocnsite.txt
+# cn_domain-s.txt cn_domain_suffix-s.txt  cn_domain-m.txt cn_ip.txt
 
-#sed -i 's/^@@//' white.txt
+#处理domain
 
-#sort nocnsite.txt -o nocnsite.txt
+jq --slurpfile domain <(cat cn_domain-s.txt | jq -R .) '.rules[0].domain += $domain' ./tem/domain.json > temp.json
 
-#sort white.txt -o white.txt
+jq --slurpfile domain_suffix <(cat cn_domain_suffix-s.txt | jq -R .) '.rules[0].domain_suffix += $domain_suffix' temp.json > cn_domain-s.json
 
-#comm -23 nocnsite.txt white.txt > temp.txt && mv temp.txt nocnsite.txt
+#jq --slurpfile domain_regex <(cat ad_domain_regex.txt | jq -R .) '.rules[0].domain_regex += $domain_regex' temp1.json > temp2.json
 
-#sed -i -e 's/^||//' -e 's/^|//' -e 's/https:\/\///' -e 's/http:\/\///' -e 's/\/.*//' nocnsite.txt
+rm temp.json
 
-#sed -e '/\*/d' -e '/^$/d' -e '/^[^.]/s/^/./' nocnsite.txt > nocn_domain_suffix.txt
+#处理ip
 
-#sed -e '/^\./d' -e '/\*/d' -e '/^$/d' nocnsite.txt > nocn_domain.txt
 
-#grep '\*' nocnsite.txt | sed -e 's/\./\\./g' -e 's/\*/\.\*/g' > nocn_domain_regex.txt
+jq --slurpfile ip_cidr <(cat cn_ip.txt | jq -R .) '.rules[0].ip_cidr += $ip_cidr' ./tem/ip.json > cn_ip-s.json
 
-#rm nocnsite.txt white.txt
+
+#处理all
+jq --slurpfile domain <(cat cn_domain-s.txt | jq -R .) '.rules[0].domain += $domain' ./tem/all.json > temp.json
+
+jq --slurpfile domain_suffix <(cat cn_domain_suffix-s.txt | jq -R .) '.rules[0].domain_suffix += $domain_suffix' temp.json > temp1.json
+
+#jq --slurpfile domain_regex <(cat ad_domain_regex.txt | jq -R .) '.rules[0].domain_regex += $domain_regex' temp1.json > temp2.json
+
+jq --slurpfile ip_cidr <(cat cn_ip.txt | jq -R .) '.rules[0].ip_cidr += $ip_cidr' temp1.json > cn_all-s.json
+
+rm temp*.json cn_domain-s.txt cn_domain_suffix-s.txt
+
+mv cn_domain-s.json ./out/s/cn_domain.json
+mv cn_ip-s.json ./out/s/cn_ip.json
+mv cn_all-s.json ./out/s/cn_all.json
+
+chmod +x ./bin/sing-box
+
+./bin/sing-box rule-set compile --output ./out/s/cn_domain.srs ./out/s/cn_domain.json
+./bin/sing-box rule-set compile --output ./out/s/cn_ip.srs ./out/s/cn_ip.json
+./bin/sing-box rule-set compile --output ./out/s/cn_all.srs ./out/s/cn_all.json
+
+
+
+
+
+
+mv cn_domain-m.txt ./out/m/cn_domain.txt
+mv cn_ip.txt ./out/m/cn_ip.txt
+chmod +x ./bin/mihomo
+
+./bin/mihomo convert-ruleset domain text ./out/m/cn_domain.txt ./out/m/cn_domain.mrs
+./bin/mihomo convert-ruleset ipcidr text ./out/m/cn_ip.txt ./out/m/cn_ip.txt.mrs
+
+
+
+
+
 
 
 
@@ -89,75 +124,67 @@ sed -Ei -e '/([0-9]{1,3}\.){2}[0-9]{1,3}/d' ad.txt
 
 sed -e '/\*/d' ad.txt > ad_domain.txt
 
-sed -e 's/^/./' ad_domain.txt > ad_domain_suffix.txt
+#符合sing-box
+sed -e 's/^/./' ad_domain.txt > ad_domain_suffix-s.txt
+#处理符合mihomo的后缀规则
+sed 's/^/\+./g' ad_domain.txt > ad_domain_suffix-m.txt
 
-grep '\*' ad.txt | sed -e 's/\./\\./g' -e 's/\?/\\?/g' -e 's/\*/\.\*/g' > ad_domain_regex.txt
 
-grep -Eo '([0-9]{1,3}\.){3}[0-9]{1,3}' ad.txt > ad_ip_cidr.txt
+#grep '\*' ad.txt | sed -e 's/\./\\./g' -e 's/\?/\\?/g' -e 's/\*/\.\*/g' > ad_domain_regex.txt
+
+#grep -Eo '([0-9]{1,3}\.){3}[0-9]{1,3}' ad.txt > ad_ip_cidr.txt
+
 
 rm ad.txt
 
 
 
-#jq --slurpfile domain <(cat cn_domain.txt | jq -R .) '.rules[0].domain += $domain' mod.json > temp.json
 
-#jq --slurpfile domain_suffix <(cat cn_domain_suffix.txt | jq -R .) '.rules[0].domain_suffix += $domain_suffix' temp.json > temp1.json
+#jq --slurpfile domain <(cat ad_domain.txt | jq -R .) '.rules[0].domain += $domain' ./tem/all.json > temp.json
 
-#jq --slurpfile ip_cidr <(cat cn_ip_cidr.txt | jq -R .) '.rules[0].ip_cidr += $ip_cidr' temp1.json > cn.json
+#jq --slurpfile domain_suffix <(cat ad_domain_suffix.txt | jq -R .) '.rules[0].domain_suffix += $domain_suffix' temp.json > temp1.json
 
-#rm cn_ip_cidr.txt cn_domain*.txt temp*.json
+#jq --slurpfile domain_regex <(cat ad_domain_regex.txt | jq -R .) '.rules[0].domain_regex += $domain_regex' temp1.json > temp2.json
 
-
-
-#jq --slurpfile domain <(cat nocn_domain.txt | jq -R .) '.rules[0].domain += $domain' mod.json > temp.json
-
-#jq --slurpfile domain_suffix <(cat nocn_domain_suffix.txt | jq -R .) '.rules[0].domain_suffix += $domain_suffix' temp.json > temp1.json
-
-#jq --slurpfile domain_regex <(cat nocn_domain_regex.txt | jq -R .) '.rules[0].domain_regex += $domain_regex' temp1.json > nocn.json
-
-#rm nocn_domain*.txt temp*.json
-
-
-
-jq --slurpfile domain <(cat ad_domain.txt | jq -R .) '.rules[0].domain += $domain' ./tem/all.json > temp.json
-
-jq --slurpfile domain_suffix <(cat ad_domain_suffix.txt | jq -R .) '.rules[0].domain_suffix += $domain_suffix' temp.json > temp1.json
-
-jq --slurpfile domain_regex <(cat ad_domain_regex.txt | jq -R .) '.rules[0].domain_regex += $domain_regex' temp1.json > temp2.json
-
-jq --slurpfile ip_cidr <(cat ad_ip_cidr.txt | jq -R .) '.rules[0].ip_cidr += $ip_cidr' temp2.json > ad_all.json
+#jq --slurpfile ip_cidr <(cat ad_ip_cidr.txt | jq -R .) '.rules[0].ip_cidr += $ip_cidr' temp2.json > ad_all.json
 
 
 
 jq --slurpfile domain <(cat ad_domain.txt | jq -R .) '.rules[0].domain += $domain' ./tem/domain.json > temp3.json
 
-jq --slurpfile domain_suffix <(cat ad_domain_suffix.txt | jq -R .) '.rules[0].domain_suffix += $domain_suffix' temp3.json > temp4.json
+jq --slurpfile domain_suffix <(cat ad_domain_suffix-s.txt | jq -R .) '.rules[0].domain_suffix += $domain_suffix' temp3.json > ad_domain-s.json
 
-jq --slurpfile domain_regex <(cat ad_domain_regex.txt | jq -R .) '.rules[0].domain_regex += $domain_regex' temp4.json > ad_domain.json
-
-
-
-jq --slurpfile ip_cidr <(cat ad_ip_cidr.txt | jq -R .) '.rules[0].ip_cidr += $ip_cidr' temp5.json > ad_ip.json
+#jq --slurpfile domain_regex <(cat ad_domain_regex.txt | jq -R .) '.rules[0].domain_regex += $domain_regex' temp4.json > ad_domain.json
 
 
-
-sed -i 's/\\r//g' ad_all.json
-sed -i 's/\\r//g' ad_domain.json
-sed -i 's/\\r//g' ad_ip.json
+#jq --slurpfile ip_cidr <(cat ad_ip_cidr.txt | jq -R .) '.rules[0].ip_cidr += $ip_cidr' temp5.json > ad_ip.json
 
 
 
-mv -f ad_all.json ./out/ad_all.json
-mv -f ad_domain.json ./out/ad_domain.json
-mv -f ad_ip.json ./out/ad_ip.json
+#sed -i 's/\\r//g' ad_all.json
+#sed -i 's/\\r//g' ad_domain.json
+#sed -i 's/\\r//g' ad_ip.json
+rm ad_domain.txt ad_domain_suffix-s.txt temp*.json
 
-
-
-rm ad_ip_cidr.txt ad_domain*.txt temp*.json
-
-
+mv ad_domain-s.json ./out/s/ad_domain.json
 
 chmod +x ./bin/sing-box
+
+./bin/sing-box rule-set compile --output ./out/s/ad_domain.srs ./out/s/ad_domain.json
+./bin/sing-box rule-set compile --output ./out/s/nocn.srs ./source/nocn.json
+
+
+
+
+
+mv ad_domain_suffix-m.txt ./out/s/ad_domain.txt
+
+chmod +x ./bin/mihomo
+
+./bin/mihomo convert-ruleset domain text ./out/m/ad_domain.txt ./out/m/ad_domain.mrs
+
+
+
 
 
 
@@ -167,15 +194,15 @@ chmod +x ./bin/sing-box
 
 #./sing-box-1.8.5-linux-amd64/sing-box rule-set compile --output nocn.srs nocn.json
 
-./bin/sing-box rule-set compile --output nocn.srs ./source/nocn.json
 
-./bin/sing-box rule-set compile --output dns.srs ./source/dns.json
 
-./bin/sing-box rule-set compile --output ./out/ad_domain.srs ./out/ad_domain.json
+#./bin/sing-box rule-set compile --output dns.srs ./source/dns.json
 
-./bin/sing-box rule-set compile --output ./out/ad_ip.srs ./out/ad_ip.json
 
-./bin/sing-box rule-set compile --output ./out/ad_all.srs ./out/ad_all.json
+
+#./bin/sing-box rule-set compile --output ./out/ad_ip.srs ./out/ad_ip.json
+
+#./bin/sing-box rule-set compile --output ./out/ad_all.srs ./out/ad_all.json
 
 
 
